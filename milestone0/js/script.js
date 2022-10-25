bff782854bc6c8d06c527a25f1f99367cc0856f5
Vergefio <riccardods@live.it>
2022-10-25 14:55:53 +0200

    add array oggetti + info

---
+++ milestone0/js/script.js
@@ -0,0 +1,31 @@
+// **MILESTONE 0:**
+// Creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+  {
+    name: 'Wayne Barnet',
+    role: 'Founder & CEO',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+  },
+];
+
+console.log(team);
