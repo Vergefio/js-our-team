522fcf2b087d9bd171a01ba1aecea476c924f379
Vergefio <riccardods@live.it>
2022-10-25 15:21:44 +0200

    add img milestone1

---
+++ milestone1/js/script.js
@@ -0,0 +1,35 @@
+// **MILESTONE 1:**
+// Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
+const row = document.querySelector('.row')
+
+const team = [
+  {
+    name: 'Wayne Barnet',
+    role: 'Founder & CEO',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    img: "img/wayne-barnett-founder-ceo.jpg"
+  },
+];
+
+console.log(team);
